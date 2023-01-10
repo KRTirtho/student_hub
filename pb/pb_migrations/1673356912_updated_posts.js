41cf2629062b77d3914e004b86973260dd1ce51a
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-01-10 19:22:33 +0600

    chore: remove comments relational mapping from posts

---
+++ pb/pb_migrations/1673356912_updated_posts.js
@@ -0,0 +1,29 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("i81wdbi331ytw3z")
+
+  // remove
+  collection.schema.removeField("bpz2szcm")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("i81wdbi331ytw3z")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bpz2szcm",
+    "name": "comments",
+    "type": "relation",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": null,
+      "collectionId": "klc6jxs27mknd61",
+      "cascadeDelete": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
